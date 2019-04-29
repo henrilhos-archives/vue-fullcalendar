911a4d7cb3b33c06302e237e39d9657b9bfe0559
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-04-29 18:01:10 -0300

    :sparkles: FullCalendar implementado

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <FullCalendar default-view="dayGridMonth" :plugins="calendarPlugins" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import FullCalendar from "@fullcalendar/vue";
+import dayGridPlugin from "@fullcalendar/daygrid";
 export default {
   name: "home",
   components: {
-    HelloWorld
+    FullCalendar
+  },
+  data() {
+    return {
+      calendarPlugins: [dayGridPlugin]
+    };
   }
 };
 </script>
+
+<style lang="scss">
+@import "~@fullcalendar/core/main.css";
+@import "~@fullcalendar/daygrid/main.css";
+</style>
